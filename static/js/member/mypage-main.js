984cc3fe02b913dbff7864f680ff9ee32585ea70
peumyora <ksbacteria@gmail.com>
2025-01-31 23:02:16 +0900

    250131_23시

--- static/js/member/mypage-main.js
+++ static/js/member/mypage-main.js
@@ -1,5 +1,22 @@
-const righttopbutton = document.querySelector("#header-right-user7");
-
-righttopbutton.addEventListener("mouseover",(e) => {
-    righttopbutton.nextElementSibling.style.display = "block";
+const righttopbutton = document.querySelector("div.header-right-user7");
+const righttopmenu = document.querySelector("div.header-right-menu8");
+NodeList.prototype.addEventListener = Array.prototype.addEventListener;
+// 우상단 마우스 이벤트트
+righttopbutton.addEventListener("mouseover", function() {
+    righttopmenu.style.display = "block";
 })
+righttopmenu.addEventListener("mouseover", function() {
+    righttopmenu.style.display = "block";
+})
+righttopbutton.addEventListener("mouseout", function() {
+    righttopmenu.style.display = "none";
+})
+righttopmenu.addEventListener("mouseout", function() {
+    righttopmenu.style.display = "none";
+})
+
+// 
+
+
+
+
