90b0e8aeb9da6b4b6ebbb2a13b4b2bb4209b3c10
peumyora <ksbacteria@gmail.com>
2025-01-31 22:17:29 +0900

    250131_22시

---
+++ static/js/member/mypage-main.js
@@ -0,0 +1,5 @@
+const righttopbutton = document.querySelector("#header-right-user7");
+
+righttopbutton.addEventListener("mouseover",(e) => {
+    righttopbutton.nextElementSibling.style.display = "block";
+})
