8e716e4db49b30a4175c7b4ebd132a0e6a062013
peumyora <ksbacteria@gmail.com>
2025-02-03 21:38:58 +0900

    0203_21:38

--- static/js/member/mypage-accountinfo1.js
+++ static/js/member/mypage-accountinfo1.js
@@ -1,13 +1,80 @@
-const editButtons = document.querySelectorAll("button.btn-modify");
+const nameCheckButton = document.getElementById("btnName");
+const nameEditButton = nameCheckButton.previousElementSibling;
+const nameCancelButton = nameCheckButton.nextElementSibling;
+const nameField = nameCheckButton.parentElement.parentElement;
+nameEditButton.addEventListener('click', ()=>{
+    console.log(nameEditButton.classList[0]);
+    console.log(nameField.classList[0]);
+    const firstDiv = nameField.firstElementChild;
+    const secondDiv = firstDiv.nextElementSibling;
+    
+    firstDiv.classList.add('inactive');
+    secondDiv.classList.remove('inactive');
+    // console.log(firstDiv.classList[1]);
+    // console.log(secondDiv.classList[1]);
+    nameCheckButton.removeAttribute('disabled');
+    nameCheckButton.nextElementSibling.removeAttribute('disabled');
+    nameCheckButton.previousElementSibling.setAttribute('disabled','true');
+    }   
+    )
+    nameCheckButton.addEventListener('click', () =>{
+    const firstDiv = nameField.firstElementChild;
+    const secondDiv = firstDiv.nextElementSibling;
+    firstDiv.classList.remove('inactive');
+    secondDiv.classList.add('inactive');
+    nameCheckButton.setAttribute('disabled','true');
+    nameCheckButton.nextElementSibling.setAttribute('disabled','true');
+    nameCheckButton.previousElementSibling.removeAttribute('disabled');
+    })
 
-editButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        const container = document.getElementsByClassName("field");
-        const firstDiv = container.lastChild.childNodes[0];
-        const secondDiv = container.lastChild.childNodes[1];
-        firstDiv.classList.add("inactive");
-        secondDiv.classList.remove("inactive");
-        console.log(firstDiv.classList);
-        console.log(secondDiv.classList);
-    });
-});
+    nameCancelButton.addEventListener('click', () =>{
+        const firstDiv = nameField.firstElementChild;
+        const secondDiv = firstDiv.nextElementSibling;
+        firstDiv.classList.remove('inactive');
+        secondDiv.classList.add('inactive');
+        nameCheckButton.setAttribute('disabled','true');
+        nameCheckButton.nextElementSibling.setAttribute('disabled','true');
+        nameCheckButton.previousElementSibling.removeAttribute('disabled');
+        })
+
+
+const birthCheckButton = document.getElementById('btnBirthDay');
+const birthEditButton = birthCheckButton.previousElementSibling;
+const birthCancelButton = birthCheckButton.nextElementSibling;
+const birthField = birthCheckButton.parentElement.parentElement;
+
+
+birthEditButton.addEventListener('click', ()=>{
+    console.log(birthEditButton.classList[0]);
+    console.log(birthField.classList[0]);
+    const firstDiv = birthField.firstElementChild;
+    const secondDiv = firstDiv.nextElementSibling;
+    
+    firstDiv.classList.add('inactive');
+    secondDiv.classList.remove('inactive');
+    // console.log(firstDiv.classList[1]);
+    // console.log(secondDiv.classList[1]);
+    birthCheckButton.removeAttribute('disabled');
+    birthCheckButton.nextElementSibling.removeAttribute('disabled');
+    birthCheckButton.previousElementSibling.setAttribute('disabled','true');
+    }   
+    )
+    birthCheckButton.addEventListener('click', () =>{
+    const firstDiv = birthField.firstElementChild;
+    const secondDiv = firstDiv.nextElementSibling;
+    firstDiv.classList.remove('inactive');
+    secondDiv.classList.add('inactive');
+    birthCheckButton.setAttribute('disabled','true');
+    birthCheckButton.nextElementSibling.setAttribute('disabled','true');
+    birthCheckButton.previousElementSibling.removeAttribute('disabled');
+    })
+
+    birthCancelButton.addEventListener('click', () =>{
+        const firstDiv = birthField.firstElementChild;
+        const secondDiv = firstDiv.nextElementSibling;
+        firstDiv.classList.remove('inactive');
+        secondDiv.classList.add('inactive');
+        birthCheckButton.setAttribute('disabled','true');
+        birthCheckButton.nextElementSibling.setAttribute('disabled','true');
+        birthCheckButton.previousElementSibling.removeAttribute('disabled');
+        })
