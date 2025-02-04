d0ceef49802c6a7da2697e0e5732ee5293e25efd
catcatsang <rksel0712@gmail.com>
2025-02-04 16:08:10 +0900

    로그인 페이지 97퍼센트

--- static/loginPage.js
+++ static/loginPage.js
@@ -31,3 +31,36 @@ password.addEventListener("blur", (e) => {
     passowrdLabel.style.fontSize = "16px";
   }
 });
+
+const tabs = document.querySelectorAll('[role="tab"]');
+const managerlogin = document.querySelector(".link_rater");
+const loging = document.querySelector(".loging");
+const tit = document.querySelectorAll(".area_intergranted_login");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    console.log("In");
+
+    tabs.forEach((t) => {
+      t.setAttribute("aria-selected", "false");
+      t.setAttribute("tabindex", "-1");
+      t.classList.remove("active");
+
+      console.log("In2");
+    });
+    tab.setAttribute("aria-selected", "true");
+    tab.setAttribute("tabindex", "0");
+    tab.classList.add("active");
+
+    if (managerlogin.style.display === "none") {
+      loging.style.display = "none";
+      tit.style.display = "none";
+      managerlogin.style.display = "block"; // 처음 클릭하면 표시
+    } else {
+      loging.style.display = "block";
+      tit.style.display = "block";
+      tit.style.display = "block";
+      managerlogin.style.display = "none";
+    }
+  });
+});
