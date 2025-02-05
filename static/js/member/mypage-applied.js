3b2c7c5126b75ae3a982d1e54e0b5eb76d099c92
KSG-gbtest <ksbacteria@gmail.com>
2025-02-05 15:53:40 +0900

    회원마이페이지 : HTML/CSS/JS 까지 완료

--- static/js/member/mypage-applied.js
+++ static/js/member/mypage-applied.js
@@ -6,21 +6,20 @@ const appliedButton = document.getElementById("applied");
 const passedButton = document.getElementById("passed");
 const hiredButton = document.getElementById("hired");
 const rejectedButton = document.getElementById("rejected");
-const lists = document.querySelectorAll("ul.topbox0 li")
+const lists = document.querySelectorAll("ul.topbox0 li");
 
 writingButton.addEventListener("click", () => {
     mainContentArea.forEach((list) => {
         if (list.classList.contains("writing-list")) {
             list.classList.remove("hidden");
             lists.forEach((li) => {
-                li.classList.remove("topbox-content1-1")
-                li.classList.add("topbox-content1")
-            })
-            writingButton.classList.add("topbox-content1-1")
-            writingButton.classList.remove("topbox-content1")
+                li.classList.remove("topbox-content1-1");
+                li.classList.add("topbox-content1");
+            });
+            writingButton.classList.add("topbox-content1-1");
+            writingButton.classList.remove("topbox-content1");
         } else {
             list.classList.add("hidden");
-            
         }
     });
 });
@@ -30,14 +29,13 @@ appliedButton.addEventListener("click", () => {
         if (list.classList.contains("applied-list")) {
             list.classList.remove("hidden");
             lists.forEach((li) => {
-                li.classList.remove("topbox-content1-1")
-                li.classList.add("topbox-content1")
-            })
-            appliedButton.classList.add("topbox-content1-1")
-            appliedButton.classList.remove("topbox-content1")
+                li.classList.remove("topbox-content1-1");
+                li.classList.add("topbox-content1");
+            });
+            appliedButton.classList.add("topbox-content1-1");
+            appliedButton.classList.remove("topbox-content1");
         } else {
-            list.classList.add("hidden"); 
-         
+            list.classList.add("hidden");
         }
     });
 });
@@ -47,15 +45,13 @@ passedButton.addEventListener("click", () => {
         if (list.classList.contains("passed-list")) {
             list.classList.remove("hidden");
             lists.forEach((li) => {
-                li.classList.remove("topbox-content1-1")
-                li.classList.add("topbox-content1")
-            })
-            passedButton.classList.add("topbox-content1-1")
-            passedButton.classList.remove("topbox-content1")
-
+                li.classList.remove("topbox-content1-1");
+                li.classList.add("topbox-content1");
+            });
+            passedButton.classList.add("topbox-content1-1");
+            passedButton.classList.remove("topbox-content1");
         } else {
-            list.classList.add("hidden"); 
-           
+            list.classList.add("hidden");
         }
     });
 });
@@ -65,14 +61,13 @@ hiredButton.addEventListener("click", () => {
         if (list.classList.contains("hired-list")) {
             list.classList.remove("hidden");
             lists.forEach((li) => {
-                li.classList.remove("topbox-content1-1")
-                li.classList.add("topbox-content1")
-            })
-            hiredButton.classList.add("topbox-content1-1")
-            hiredButton.classList.remove("topbox-content1")
+                li.classList.remove("topbox-content1-1");
+                li.classList.add("topbox-content1");
+            });
+            hiredButton.classList.add("topbox-content1-1");
+            hiredButton.classList.remove("topbox-content1");
         } else {
-            list.classList.add("hidden"); 
-         
+            list.classList.add("hidden");
         }
     });
 });
@@ -82,47 +77,44 @@ rejectedButton.addEventListener("click", () => {
         if (list.classList.contains("rejected-list")) {
             list.classList.remove("hidden");
             lists.forEach((li) => {
-                li.classList.remove("topbox-content1-1")
-                li.classList.add("topbox-content1")
-            })
-            rejectedButton.classList.add("topbox-content1-1")
-            rejectedButton.classList.remove("topbox-content1")
+                li.classList.remove("topbox-content1-1");
+                li.classList.add("topbox-content1");
+            });
+            rejectedButton.classList.add("topbox-content1-1");
+            rejectedButton.classList.remove("topbox-content1");
         } else {
-            list.classList.add("hidden"); 
-            
+            list.classList.add("hidden");
         }
     });
 });
+const filterButtonArea = document.querySelector("div.applied-list0-filter");
+const filterButton = document.querySelector("button.btn-text");
+const filterArrow = document.querySelector("svg.arrow");
+const filterList = document.querySelector("div.filter-list");
+const filterBackground = document.querySelector("div.filter-background");
+// filterButton.addEventListener("click", () => {
+//     if (filterButton.getAttribute("aria-expanded") == "false") {
+//         filterButton.setAttribute("aria-expanded", "true");
+//         filterArrow.classList.add("arrow-up");
+//         filterList.classList.remove("hidden");
+//         filterBackground.classList.remove("hidden");
+//     } else {
+//         filterButton.setAttribute("aria-expanded", "false");
+//         filterArrow.classList.remove("arrow-up");
+//         filterList.classList.add("hidden");
+//         filterBackground.classList.add("hidden");
+//     }
+// });
 
-const filterButton = document.querySelector("button.btn-text")
-const filterArrow = document.querySelector("svg.arrow")
-const filterList = document.querySelector("div.filter-list")
-const filterBackground = document.querySelector("div.filter-background")
-filterButton.addEventListener('click', () => {
-    if (filterButton.getAttribute("aria-expanded") == "false") {
-        filterButton.setAttribute("aria-expanded", "true")
-        filterArrow.classList.add("arrow-up")
-        filterList.classList.remove("hidden")
-        filterBackground.classList.remove("hidden")
-    }
-    else {
-        filterButton.setAttribute("aria-expanded", "false")
-        filterArrow.classList.remove("arrow-up")
-        filterList.classList.add("hidden")
-        filterBackground.classList.add("hidden")
-    }
-})
-
-
-const appliedChoice = document.querySelectorAll("button.peer")
-const optApplied = document.getElementById("APPLIED")
-const optRead = document.getElementById("READ")
-const optUnread = document.getElementById("UNREAD")
-const optCanceled = document.getElementById("CANCELED")
-const optSpan = document.querySelector("button.btn-text > span")
-appliedChoice.forEach(choice => {
-    choice.addEventListener('click', () => {
-        console.log(choice.innerText)
+const appliedChoice = document.querySelectorAll("button.peer");
+const optApplied = document.getElementById("APPLIED");
+const optRead = document.getElementById("READ");
+const optUnread = document.getElementById("UNREAD");
+const optCanceled = document.getElementById("CANCELED");
+const optSpan = document.querySelector("button.btn-text > span");
+appliedChoice.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        console.log(choice.innerText);
         switch (choice.innerText) {
             case "전체":
                 optSpan.innerText = "전체";
@@ -137,6 +129,28 @@ appliedChoice.forEach(choice => {
                 optSpan.innerText = "지원취소";
                 break;
         }
-    })
+    });
     // 나중에 서버랑 연동해서 조건에 해당하는 공고목록 띄우도록 바꿔야 함.
-})
+});
+
+document.addEventListener("click", function (e) {
+    console.log(e.target);
+    if (filterButtonArea.contains(e.target)) {
+        if (filterButton.getAttribute("aria-expanded") == "false") {
+            filterButton.setAttribute("aria-expanded", "true");
+            filterArrow.classList.add("arrow-up");
+            filterList.classList.remove("hidden");
+            filterBackground.classList.remove("hidden");
+        } else {
+            filterButton.setAttribute("aria-expanded", "false");
+            filterArrow.classList.remove("arrow-up");
+            filterList.classList.add("hidden");
+            filterBackground.classList.add("hidden");
+        }
+    } else if (!filterList.contains(e.target)) {
+        filterButton.setAttribute("aria-expanded", "false");
+        filterArrow.classList.remove("arrow-up");
+        filterList.classList.add("hidden");
+        filterBackground.classList.add("hidden");
+    }
+});
