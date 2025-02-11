a3ffb51317250339f72697cc75503bbc32a7eade
jfk6725 <jfk6725@gmai.com>
2025-02-11 22:27:30 +0900

    사이드바 최초 작성

--- static/js/aside/applicantmanagement.js
+++ static/js/aside/applicantmanagement.js
@@ -1,28 +1,35 @@
-// 포지션 전체 클릭되었을 때 진하게 여리게
-document.querySelector(".title.all").addEventListener((e) => {});
+// 3개 포지션 버튼 클릭 이벤트
+document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", (e) => {
+        // 클릭되어 있던 버튼 원상복구
+        document.querySelectorAll("button").forEach((button) => {
+            button.classList.remove("clicked");
+        });
 
-// 다른 포지션이 클릭되었을 때
-function toggleArrow(element) {
-    // 모든 dropdown menu 없애기
-    document.querySelectorAll("li").forEach((e) => {
-        e.remove();
+        // 이미 만들어져 있는 dropdown menu 모두 삭제 (원상복구)
+        document.querySelectorAll("li").forEach((e) => {
+            e.remove();
+        });
+
+        // 버튼의 배경색과 글자색이 바뀌도록 변경
+        if (e.target.tagName == "SPAN") {
+            // 진행중 포지션과 마감된 포지션은 SPAN이 클릭 됨.
+            e.target.closest("button").classList.add("clicked");
+
+            // 형제 ul의 dropdown menu 생성
+            e.target.closest("button").nextElementSibling.innerHTML = `
+                                                    <li class="circle">프론트 웹 개발자</li>
+                                                    <li class="circle">PHP 웹 개발자(신입)</li>
+                                                    <li class="circle">iOS 모바일앱 개발자</li>`;
+        } else {
+            e.target.classList.add("clicked");
+        }
     });
-    // 모든 화살표를 위로 향하게 하기
-    document.querySelectorAll(".arrow").forEach((e) => {
-        e.classList.remove("down");
-    });
-    // 클릭된 부분에 dropdown menu 생성
-    if (!element.querySelector(".arrow").classList.contains("down")) {
-        element.nextElementSibling.innerHTML = `
-                <li>프론트 웹 개발자</li>
-                <li>PHP 웹 개발자(신입)</li>
-                <li>iOS 모바일앱 개발자</li>`;
-        element.querySelector(".arrow").classList.toggle("down");
-    }
-}
+});
 
 // dropdown 메뉴가 클릭됬을 때 이벤트 발생시키기
 document.querySelectorAll("ul").forEach((ul) => {
+    console.log(" for each ul ");
     ul.addEventListener("click", (e) => {
         // 클릭된 요소가 li인 경우
         if (e.target.tagName === "LI") {
